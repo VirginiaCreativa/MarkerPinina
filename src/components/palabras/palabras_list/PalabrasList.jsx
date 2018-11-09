b000ebb1f7beea70b34f66c8dfef86aa26d9b009
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-09 12:08:13 -0500

    Create Firebase JSON and Setting PalabrasLists

--- src/components/palabras/palabras_list/PalabrasList.jsx
+++ src/components/palabras/palabras_list/PalabrasList.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PalabraItem from './palabra_item/PalabraItem';
 
 class PalabrasLists extends Component {
-  state = {};
+  state = {
+    palabras: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://markerpinina.firebaseio.com/palabras.json')
+      .then(response => {
+        this.setState({ palabras: response.data });
+      })
+      // eslint-disable-next-line no-console
+      .catch(error => console.log(error));
+  }
 
   render() {
-    return <PalabraItem />;
+    const { palabras } = this.state;
+    return (
+      <>
+        {palabras.map(palabra => (
+          <PalabraItem key={palabra.id} {...palabra} />
+        ))}
+      </>
+    );
   }
 }
 export default PalabrasLists;
