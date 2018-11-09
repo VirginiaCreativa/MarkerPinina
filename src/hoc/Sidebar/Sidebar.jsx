06c4ba52e426b4febc952c9963d2e8f32b26a19f
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-08 19:51:08 -0500

    DS header listo

--- src/hoc/Sidebar/Sidebar.jsx
+++ src/hoc/Sidebar/Sidebar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import classes from './Sidebar.scss';
 
 const Sidebar = () => (
-  <div>
-    <h2>Sidebar</h2>
-    <ul>
-      <li>Palabras</li>
-      <li>Signficiados</li>
-      <li>About</li>
-    </ul>
+  <div className={classes.Sidebar}>
+    <NavLink to="/palabras" activeClassName="selected" className="linkActive">
+      <i className="bx bx-font-color" />
+    </NavLink>
+    <NavLink
+      to="/significados"
+      activeClassName="selected"
+      className="linkActive"
+    >
+      <i className="bx bx-pin" />
+    </NavLink>
   </div>
 );
 
-export default Sidebar;
+export default withRouter(Sidebar);
