d895889b19c6dc0d1c0eb8f69c1fa17cf4f3f141
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-17 21:50:46 -0500

    Change design header

--- src/hoc/Navigations/Signed/SignedInLinks.jsx
+++ src/hoc/Navigations/Signed/SignedInLinks.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
-import { NavLink, Link, withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import classes from './SignedInLinks.scss';
 
 const SignedInLinks = () => (
   <>
-    <ul className={['nav', classes.SignedInLinks].join(' ')}>
-      <button className={classes.btnCreate} type="button">
-        <i className="bx bx-plus" />
-        <div className={classes.Menus}>
-          <div className={classes.Triangule} />
-          <ul>
-            <li>
-              <Link to="/palabras">
-                <i className="bx bx-pin" />
-                Palabra
-              </Link>
-            </li>
-            <li>
-              <Link to="/enseñanza">
-                <i className="bx bx-book-bookmark" />
-                Notebook
-              </Link>
-            </li>
-            <li>
-              <Link to="/proyecto">
-                <i className="bx bx-idea" />
-                Proyecto
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </button>
-      <NavLink to="/" className={classes.Notification}>
-        <i className="bx bx-notification" />
-      </NavLink>
-      <NavLink to="/">
-        <i className="bx bx-log-out" />
-      </NavLink>
-      <NavLink to="/" className={classes.User}>
-        <div className={classes.Circle}>VS</div>
-        <p>Virginia Velásquez</p>
-      </NavLink>
-    </ul>
+    <div className={['nav', classes.SignedInLinks].join(' ')}>
+      <div className={classes.Grid}>
+        <button className={classes.btnCreate} type="button">
+          <i className="bx bx-plus-circle" />
+          Crear nuevos
+          <div className={classes.Menus}>
+            <div className={classes.Triangule} />
+            <ul>
+              <li>
+                <Link to="/significados">
+                  <i className="bx bx-pin" />
+                  Signficados
+                </Link>
+              </li>
+              <li>
+                <Link to="/notascornell">
+                  <i className="bx bx-book-bookmark" />
+                  Notas Cornell
+                </Link>
+              </li>
+              <li>
+                <Link to="/taduccion">
+                  <i className="bx bx-transfer" />
+                  Traduccion
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </button>
+      </div>
+      <div className={classes.Grid}>
+        <Link to="/" className={classes.User}>
+          <div className={classes.Circle}>VS</div>
+          <p>Virginia Velásquez</p>
+          <i className="bx bx-chevron-down" />
+        </Link>
+      </div>
+    </div>
   </>
 );
 
