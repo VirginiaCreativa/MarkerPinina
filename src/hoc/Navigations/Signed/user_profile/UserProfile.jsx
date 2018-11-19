3a308e1326c2eb56acbd62d09deab7b0e02cc118
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-18 20:30:41 -0500

    Setting btn UserMenu in SignedInLink

--- src/hoc/Navigations/Signed/user_profile/UserProfile.jsx
+++ src/hoc/Navigations/Signed/user_profile/UserProfile.jsx
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './UserProfile.scss';
 
-const UserProfile = ({ userSign }) => (
-  <div className={classes.UserProfile}>
-    {userSign ? (
-      <div className={classes.CircleImg} />
-    ) : (
-      <div className={classes.CircleAnomy}>VS</div>
-    )}
+class UserProfile extends Component {
+  state = {
+    isMenu: false,
+  };
 
-    <p>Virginia Velásquez</p>
-    <button type="button">
-      <i className="bx bx-chevron-down" />
-    </button>
-  </div>
-);
+  handleShowMenu = () => {
+    this.setState({ isMenu: true });
+  };
+
+  handleOnMouseMenu = e => {
+    this.setState({ isMenu: true });
+  };
+
+  handleHideMenu = e => {
+    // console.log(e.clientX);
+    this.setState({ isMenu: false });
+  };
+
+  render() {
+    const { userSign } = this.props;
+    const { isMenu } = this.state;
+    console.log(this.props);
+    return (
+      <>
+        <div className={classes.UserProfile}>
+          {userSign ? (
+            <div className={classes.CircleImg} />
+          ) : (
+            <div className={classes.CircleAnomy}>VS</div>
+          )}
+
+          <p>Virginia Velásquez</p>
+          <button
+            type="button"
+            className={classes.btnMenuUser}
+            onClick={this.handleShowMenu}
+          >
+            <i className="bx bx-chevron-down" />
+          </button>
+          <div>
+            {isMenu && (
+              <div
+                className={classes.isMenuOpen}
+                onMouseLeave={this.handleHideMenu}
+              >
+                <ul>
+                  <li>
+                    <a href="">item</a>
+                  </li>
+                  <li>
+                    <a href="">item</a>
+                  </li>
+                  <li>
+                    <a href="">item</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default UserProfile;
