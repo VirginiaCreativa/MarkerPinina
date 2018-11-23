4b0f4c7ec49fe7580921f579d954800eb4d91b6c
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-23 10:15:25 -0500

    Font Gilroy

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 import '../styles/variables.scss';
 import '../styles/imports.scss';
 import '../styles/global.scss';
+import '../styles/fonts.css';
 
 // require('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=10000&reload=true');
